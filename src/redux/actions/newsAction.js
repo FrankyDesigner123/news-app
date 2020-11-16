b5e84f591950e5a601bad0d61629824517082d35
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-11-16 19:34:32 +0100

    Making HTTP requests

--- src/redux/actions/newsAction.js
+++ src/redux/actions/newsAction.js
@@ -2,8 +2,17 @@ export const FETCH_ARTICLES = 'FETCH_ARTICLES';
 export const ADD_FAVORITES = 'ADD_FAVORITES';
 
 export const fetchArticles = () => {
-	return {
-		type: FETCH_ARTICLES,
-		payload: { id: 1, title: 'Sport news', description: 'random desc.' },
+	return async (dispatch) => {
+		// add logic to fetch data
+		const result = await fetch(
+			'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=40dd709612784220b441caa95e930651'
+		);
+
+		const resultData = await result.json();
+
+		dispatch({
+			type: FETCH_ARTICLES,
+			payload: { id: 1, title: 'Sport news', description: 'random desc.' },
+		});
 	};
 };
